09f39f0a8976871f4f198b325f102f086ba05838
Aathish101 <aathishvs2@gmail.com>
2025-11-03 10:43:10 +0530

    Add files via upload

---
+++ Replateo/firebase/irebaseConfig.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "replateo.firebaseapp.com",
+  projectId: "replateo",
+  storageBucket: "replateo.appspot.com",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
